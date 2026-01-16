a5a05683f20d8f309fcf73e0a9e328baeed1d6a4
mohamedreda22 <mohamed_aboreda_2020@yahoo.com>
2026-01-16 17:07:40 +0200

    0

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@ reportWebVitals();
 
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
-import App from "./components/App";
+import "./index.css";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 createRoot(rootElement).render(
